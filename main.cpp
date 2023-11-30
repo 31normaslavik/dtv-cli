4abc451e08418bcb8c48de9014035aa0b2348d82
I'm Pancake <slavik15971597@gmail.com>
2023-11-30 12:54:49 +0300

    fix

--- main.cpp
+++ main.cpp
@@ -65,9 +65,26 @@ int main(int argc, char** argv) try {
         urls.emplace_back(argv[i]);
 
     std::string temp_dir{temp_dir_generate("dtv-cli")};
-    //std::string temp_dir{"dtv-cli"};
 
-    fs::path path_to_save{argv[2]};
+    std::string path_argv{argv[2]};
+    fs::path path_to_save;
+
+    if(path_argv.starts_with('.') && path_argv.size() == 1){
+        path_to_save = std::getenv("PWD");
+    }else
+    if(path_argv.starts_with("./")){
+        path_to_save = std::string(std::getenv("PWD")) + "/" + path_argv.substr(2);
+    }else
+    if(!path_argv.starts_with("/")){
+        path_to_save = std::string(std::getenv("PWD")) + "/" + path_argv;
+    }else
+    {
+        path_to_save = path_argv;
+    }
+
+    if(path_to_save.empty())
+        throw std::runtime_error("Path to save file is empty");
+
     if(!fs::exists(path_to_save))
         fs::create_directories(path_to_save);
 
@@ -98,8 +115,8 @@ int main(int argc, char** argv) try {
         error_code ec;
         value json_data = parse( file , ec);
         if(ec){
-        throw std::runtime_error(std::to_string(ec.value()) + " " + ec.message());
-    }
+            throw std::runtime_error(std::to_string(ec.value()) + " " + ec.message());
+        }
 
         std::vector<video_info> playlist;
 
@@ -110,106 +127,104 @@ int main(int argc, char** argv) try {
 
         if(p.at("_type").is_string() && p.at("_type").as_string() == "playlist"){
 
-        std::int64_t playlist_count = json_data.if_object()->at("playlist_count").as_int64();
+            std::int64_t playlist_count = json_data.if_object()->at("playlist_count").as_int64();
 
-        cout<<"Videos in playlist: "<< playlist_count<<endl;
+            cout<<"Videos in playlist: "<< playlist_count<<endl;
 
-        for( std::int64_t i{} ; i < playlist_count ; ++i){
+            for( std::int64_t i{} ; i < playlist_count ; ++i){
+                video_info video {
+                    p.at("entries").if_array()->at(i).at("original_url").as_string().c_str(),
+                    p.at("entries").if_array()->at(i).at("display_id").as_string().c_str(),
+                    p.at("entries").if_array()->at(i).at("title").as_string().c_str(),
+                    p.at("title").as_string().c_str(),
+                    p.at("entries").if_array()->at(i).at("playlist_index").as_int64()
+                };
+
+                playlist.emplace_back(std::move(video));
+            }
+
+
+        } else if(p.at("_type").is_string() && p.at("_type").as_string() == "video"){
             video_info video {
-                            p.at("entries").if_array()->at(i).at("original_url").as_string().c_str(),
-                            p.at("entries").if_array()->at(i).at("display_id").as_string().c_str(),
-                            p.at("entries").if_array()->at(i).at("title").as_string().c_str(),
-                            p.at("title").as_string().c_str(),
-                            p.at("entries").if_array()->at(i).at("playlist_index").as_int64()
+                p.at("original_url").as_string().c_str(),
+                p.at("display_id").as_string().c_str(),
+                p.at("title").as_string().c_str(), "", 0
             };
 
             playlist.emplace_back(std::move(video));
         }
-
-
-        } else if(p.at("_type").is_string() && p.at("_type").as_string() == "video"){
-        video_info video {
-                        p.at("original_url").as_string().c_str(),
-                        p.at("display_id").as_string().c_str(),
-                        p.at("title").as_string().c_str(), "", 0
-        };
-
-        playlist.emplace_back(std::move(video));
-    }
         else throw std::runtime_error("Non valide json file");
 
-         for(auto&i : playlist){
-             std::string audio, video, translated_audio;
-           if(yt_dlp_command_run(i.original_url,format_video(argv[4])) != 0){ // скачивание видео
-                 throw std::runtime_error("yt-dlp launched incorrectly");
-             }
-           if(vot_cli_command_run(i.original_url) != 0){ // скачивание аудио yandex api
-                 throw std::runtime_error("vot-cli launched incorrectly");
-             }
+        for(auto&i : playlist){
+            std::string audio, video, translated_audio;
+            if(yt_dlp_command_run(i.original_url,format_video(argv[4])) != 0){ // скачивание видео
+                throw std::runtime_error("yt-dlp launched incorrectly");
+            }
+            if(vot_cli_command_run(i.original_url) != 0){ // скачивание аудио yandex api
+                throw std::runtime_error("vot-cli launched incorrectly");
+            }
             for (const auto &entry : fs::directory_iterator(tmp)) {
                 if (entry.is_regular_file() && entry.path().string().find(i.display_id) != std::string::npos) {
 
-                    cout<<entry.path().filename() << " условие выполнено"<<endl;
-                    switch (extension(entry.path().extension())) {
-                      case EXTENSION::MP4:
-                      case EXTENSION::WEBM:
-                         video = entry.path().filename();
-                         break;
-                      case EXTENSION::MP3:
-                         translated_audio = entry.path().filename();
-                         break;
-                      case EXTENSION::M4A:
-                         audio = entry.path().filename();
-                      default:
-                         break;
+                    switch (extension(entry.path().extension().string())) {
+                    case EXTENSION::MP4:
+                    case EXTENSION::WEBM:
+                        video = entry.path().filename().string();
+                        break;
+                    case EXTENSION::MP3:
+                        translated_audio = entry.path().filename().string();
+                        break;
+                    case EXTENSION::M4A:
+                        audio = entry.path().filename().string();
+                    default:
+                        break;
                     }
-                } else
-                    cout<<entry.path().filename()<< " условие не выполнено"<<endl;
-
+                }
             }
 
-           if(audio.empty() || video.empty() || translated_audio.empty())
-                 throw std::runtime_error("one or more files needed for ffmpeg not found");
+            if(audio.empty() || video.empty() || translated_audio.empty())
+                throw std::runtime_error("one or more files needed for ffmpeg not found");
 
-           if(ffmpeg_command_run(video, translated_audio, audio, i.display_id + ".mp4") != 0)
-                 throw std::runtime_error("ffmpeg launched incorrectly");
+            if(ffmpeg_command_run(video, translated_audio, audio, i.display_id + ".mkv") != 0)
+                throw std::runtime_error("ffmpeg launched incorrectly");
 
-           std::istringstream ss{i.title};
-           for(std::string temp, newstring ; ss >> temp;){
-               for(auto pos = temp.find_first_of("/.,\\"); pos != std::string::npos ; pos = temp.find_first_of("/.,\\") ){
-                     temp.erase(pos,1);
-                 }
-               newstring += temp.append(" ");
-                 i.title = newstring;
-                 i.title.erase(i.title.size() - 1);
-           }
-           if(fs::exists(i.display_id + ".mp4")) {
-               cout<< tmp.string() << " " << path_to_save.string() << endl;
-               if(fs::copy_file(tmp / std::string(i.display_id + ".mp4"), path_to_save / std::string(i.title + ".mp4"),
-                                   fs::copy_options::update_existing)){
-                   cout<< "file: " << i.display_id + ".mp4" << " sussed copy to: " << path_to_save.relative_path() << endl;
-                   for (const auto &entry : fs::directory_iterator(tmp)) {
-                         if (entry.is_regular_file() && entry.path().string().find(i.display_id) != std::string::npos) {
-                           switch (extension(entry.path().extension())) {
-                             case EXTENSION::MP4:
-                             case EXTENSION::M4A:
-                             case EXTENSION::MKV:
-                             case EXTENSION::WEBM:
-                             case EXTENSION::MP3:
-                                 fs::remove(entry.path().filename());
-                                 cout<< entry.path().filename() << " was removed" << endl;
-                                 break;
-                             default:
-                                 break;
-                             }
-                         }
-                     }
-                 }
-             }
-         }
+            std::istringstream ss{i.title};
+            for(std::string temp, newstring ; ss >> temp;){
+                std::string exclude_chars{"/.,\\:|# "};
 
+                for(auto pos = temp.find_first_of(exclude_chars); pos != std::string::npos ; pos = temp.find_first_of(exclude_chars) ){
+                    temp.erase(pos,1);
+                }
 
-
+                newstring += temp.append("_");
+                i.title = newstring;
+                i.title.erase(i.title.size() - 1);
+            }
+            if(fs::exists(i.display_id + ".mkv")) {
+                cout<< tmp.string() << " " << path_to_save.string() << endl;
+                if(fs::copy_file(tmp / std::string(i.display_id + ".mkv"), path_to_save / std::string(
+                                                                                (i.playlist_index == 0 ? "" : std::to_string(i.playlist_index) + "_") + i.title + ".mkv"),
+                                  fs::copy_options::update_existing)){
+                    cout<< "file: " << i.display_id + ".mkv" << " sussed copy to: " << path_to_save.relative_path() << endl;
+                    for (const auto &entry : fs::directory_iterator(tmp)) {
+                        if (entry.is_regular_file() && entry.path().string().find(i.display_id) != std::string::npos) {
+                            switch (extension(entry.path().extension().string())) {
+                            case EXTENSION::MP4:
+                            case EXTENSION::M4A:
+                            case EXTENSION::MKV:
+                            case EXTENSION::WEBM:
+                            case EXTENSION::MP3:
+                                fs::remove(entry.path().filename());
+                                cout<< entry.path().filename() << " was removed" << endl;
+                                break;
+                            default:
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 
     cout<<fs::remove_all(tmp)<<": files is remove in "<< tmp <<endl;
