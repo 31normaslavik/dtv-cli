bd7fa27258259edb8467cace1294d38c35b944d9
i-see-you-pancake <slavik15971597@gmail.com>
2024-03-16 07:26:52 +0300

    add command_line_parser function with boost::program_options

--- main.cpp
+++ main.cpp
@@ -1,58 +1,61 @@
 #include "downloader.h"
 #include <iostream>
-#include <set>
-#include "presentation.h"
+//#include <set>
+//#include "presentation.h"
+#include "command_line_parser.h"
 
 // dtv-cli --output ./ --format [ mp4[1080,720,480], webm[1080,720,480] ]  urls
 int main(int argc, char** argv) try {
     using std::cout;
     using std::endl;
 
-    if (argc < 5) {
-        std::cerr << "dtv-cli --output ./ "
-                "--format [ mp4[_4320 _2160 _1440 _1080 _720 _480 _360] "
-                "webm[_2160 _1440 _1080 _720 _480 _360] ] [_30 _30_10 _60 _60_10]  urls\n" << endl;
-        std::cerr << "Format 480p 30 fps" << endl;
-        std::cerr << "dtv-cli --output ./playlist4 --format mp4_480_30 \"url1\" \"url2\"\n" << endl;
+    ParserLine parse = command_line_parser(argc, argv);
 
-        std::cerr << "Format 1080p 60 fps" << endl;
-        std::cerr << "dtv-cli --output ./playlist1 --format mp4_1080_60 \"url\"\n" << endl;
+    // if (argc < 5) {
+    //     std::cerr << "dtv-cli --output ./ "
+    //                  "--format [mp4 webm] [_4320 _2160 _1440 _1080 _720 _480 _360] "
+    //                  "[_30 _30_10 _60 _60_10]  urls\n" << std::endl;
+    //     std::cerr << "Format 480p 30 fps" << std::endl;
+    //     std::cerr << "dtv-cli --output ./playlist4 --format mp4_480_30 \"url1\" \"url2\"\n" << std::endl;
 
-        std::cerr << "Format 2160p 60 fps HDR" << endl;
-        std::cerr << "dtv-cli --output ./playlist4 --format mp4_2160_60_10 \"url1\" \"url2\"\n" << endl;
+    //     std::cerr << "Format 1080p 60 fps" << std::endl;
+    //     std::cerr << "dtv-cli --output ./playlist1 --format mp4_1080_60 \"url\"\n" << std::endl;
 
-        std::cerr << "Format 2160p 60 fps HDR" << endl;
-        std::cerr << "dtv-cli --output ./playlist2 --format webm_2160_60_10 \"url1\" \"url2\" \"url3\"\n" << endl;
-        return 0;
-    }
+    //     std::cerr << "Format 2160p 60 fps HDR" << std::endl;
+    //     std::cerr << "dtv-cli --output ./playlist4 --format mp4_2160_60_10 \"url1\" \"url2\"\n" << std::endl;
 
-    if (std::string(argv[1]) != "--output") {
-        std::cerr << "arg1 --output" << endl;
-        return 0;
-    }
+    //     std::cerr << "Format 2160p 60 fps HDR" << std::endl;
+    //     std::cerr << "dtv-cli --output ./playlist2 --format webm_2160_60_10 \"url1\" \"url2\" \"url3\"\n" << endl;
+    //     return 0;
+    // }
 
-    if (std::string(argv[2]).find("https://") != std::string::npos) {
-        std::cerr << "arg2 path incorrect" << endl;
-        return 0;
-    }
+    // if (std::string(argv[1]) != "--output") {
+    //     std::cerr << "arg1 --output" << endl;
+    //     return 0;
+    // }
 
-    if (std::string(argv[3]) != "--format") {
-        std::cerr << "arg3 --format" << endl;
-        return 0;
-    }
+    // if (std::string(argv[2]).find("https://") != std::string::npos) {
+    //     std::cerr << "arg2 path incorrect" << endl;
+    //     return 0;
+    // }
 
-    if(format_video_argv(argv[4]) == FORMAT_VIDEO::UNKNOWNV){
-        std::cerr<<"arg4 format incorrect"<<endl;
-        return 0;
-    }
+    // if (std::string(argv[3]) != "--format") {
+    //     std::cerr << "arg3 --format" << endl;
+    //     return 0;
+    // }
 
-    std::set<std::string> urls;
+    // if(format_video_argv(argv[4]) == FORMAT_VIDEO::UNKNOWNV){
+    //     std::cerr<<"arg4 format incorrect"<<endl;
+    //     return 0;
+    // }
 
-    for (size_t i{5}; i < argc; ++i)
-        urls.insert(argv[i]);
+    // std::set<std::string> urls;
+
+    // for (size_t i{5}; i < argc; ++i)
+    //     urls.insert(argv[i]);
 
 
-    dtv::Downloader d(urls, argv[2], argv[4]);
+    dtv::Downloader d(parse.urls, parse.pathToSave, parse.format);
 
     d.download_videos_with_translate();
     return 0;
