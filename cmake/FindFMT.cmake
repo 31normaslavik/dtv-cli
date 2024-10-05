c7f7728ee326afa6c8409241adb2babc3e66c9d5
i-see-you-pancake <bugurt2018@yandex.ru>
2024-10-05 10:19:53 +0300

    FetchContent fmt lib

--- cmake/FindFMT.cmake
+++ cmake/FindFMT.cmake
@@ -1,14 +1,9 @@
-# include(FetchContent)
+include(FetchContent)
 
-# FetchContent_Declare(
-#   fmt
-#   GIT_REPOSITORY https://github.com/fmtlib/fmt
-#   GIT_TAG        0c9fce2ffefecfdce794e1859584e25877b7b592 # 11.0.2
-# )
-# FetchContent_MakeAvailable(fmt)
-
-
-find_package(fmt)
-# if(NOT FMT_FOUND)
-#     message(FATAL_ERROR "fmt not found")
-# endif()
+FetchContent_Declare(
+  fmt
+  GIT_REPOSITORY https://github.com/fmtlib/fmt
+  GIT_TAG        0c9fce2ffefecfdce794e1859584e25877b7b592 # 11.0.2
+  FIND_PACKAGE_ARGS NAMES fmt
+)
+FetchContent_MakeAvailable(fmt)
