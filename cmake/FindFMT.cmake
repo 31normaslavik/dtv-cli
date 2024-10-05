3f0bac914ace6237c7dbafeef2a74d58d55771ba
i-see-you-pancake <bugurt2018@yandex.ru>
2024-10-05 23:04:01 +0300

    fix: fatal error: fmt/format.h: No such file or directory

--- cmake/FindFMT.cmake
+++ cmake/FindFMT.cmake
@@ -2,8 +2,8 @@ include(FetchContent)
 
 FetchContent_Declare(
   fmt
-  GIT_REPOSITORY https://github.com/fmtlib/fmt
+  GIT_REPOSITORY https://github.com/fmtlib/fmt.git
   GIT_TAG        0c9fce2ffefecfdce794e1859584e25877b7b592 # 11.0.2
-  FIND_PACKAGE_ARGS NAMES fmt
+  # FIND_PACKAGE_ARGS NAMES fmt
 )
 FetchContent_MakeAvailable(fmt)
