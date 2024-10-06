4b9f17dcd972035a3f15fde4a6f1b2e08bf56653
i-see-you-pancake <bugurt2018@yandex.ru>
2024-10-06 20:24:51 +0300

    formating output

--- cmake/CheckDepedencies.cmake
+++ cmake/CheckDepedencies.cmake
@@ -1,61 +1,63 @@
 
 
 find_program(MAKE make)
-message("MAKE: ${MAKE}")
+message("MAKE:      ${MAKE}")
 
 find_program(PYTHON3 python3)
-message("PYTHON3: ${PYTHON3}")
+message("PYTHON3:   ${PYTHON3}")
 
 find_program(NODE node)
-message("NODE: ${NODE}")
+message("NODE:      ${NODE}")
 
 find_program(VOTCLI vot-cli)
-message("VOTCLI: ${VOTCLI}")
+message("VOTCLI:    ${VOTCLI}")
 
 find_program(YTDLP yt-dlp)
-message("YTDLP: ${YTDLP}")
+message("YTDLP:     ${YTDLP}")
 
 find_program(APT apt)
-message("APT: ${APT}")
+message("APT:       ${APT}")
 
 find_program(APTGET apt-get)
-message("APTGET: ${APTGET}")
+message("APTGET:    ${APTGET}")
 
+# only in manjaro
 find_program(PAMAC pamac)
-message("PAMAC: ${PAMAC}")
+message("PAMAC:     ${PAMAC}")
 
 find_program(PACMAN pacman)
-message("PACMAN: ${PACMAN}")
+message("PACMAN:    ${PACMAN}")
 
 find_program(YAY yay)
-message("YAY: ${YAY}")
+message("YAY:       ${YAY}")
 
 find_program(FLATPAK flatpak)
-message("FLATPAK: ${FLATPAK}")
+message("FLATPAK:   ${FLATPAK}")
 
 find_program(RPM rpm)
-message("RPM: ${RPM}")
+message("RPM:       ${RPM}")
 
 find_program(ZYPPER zypper)
-message("ZYPPER: ${ZYPPER}")
+message("ZYPPER:    ${ZYPPER}")
 
 find_program(PORTAGE portage)
-message("PORTAGE: ${PORTAGE}")
+message("PORTAGE:   ${PORTAGE}")
 
 find_program(VCPKG vcpkg)
-message("VCPKG: ${VCPKG}")
+message("VCPKG:     ${VCPKG}")
 
 find_program(PIP pip)
-message("PIP: ${PIP}")
+message("PIP:       ${PIP}")
 
 find_program(CONAN conan)
-message("CONAN: ${CONAN}")
+message("CONAN:     ${CONAN}")
 
 find_program(BUILD2 build2)
-message("BUILD2: ${BUILD2}")
+message("BUILD2:    ${BUILD2}")
 
 find_program(CPM cpm)
-message("CPM: ${CPM}")
+message("CPM:       ${CPM}")
 
+# предустановлен в ubuntu
 find_program(SNAP snap)
-message("SNAP: ${SNAP}")
+message("SNAP:      ${SNAP}")
