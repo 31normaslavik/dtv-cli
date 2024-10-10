134d6e8b9a16d561d67bf8a301650639fe96f8e4
i-see-you-pancake <bugurt2018@yandex.ru>
2024-10-11 00:14:07 +0300

    add check module install

--- cmake/FindFMT.cmake
+++ cmake/FindFMT.cmake
@@ -3,16 +3,21 @@ set(CMAKE_FIND_PACKAGE_SORT_ORDER NATURAL)
 set(CMAKE_FIND_PACKAGE_SORT_DIRECTION DEC)
 set(FETCHCONTENT_QUIET FALSE)
 
-find_package(fmt 9.1 REQUIRED)
+find_package(fmt 9.1)
+if(fmt_FOUND)
+  message("FMT FOUND VERSION: ${fmt_VERSION}")
+else()
 
-# include(FetchContent)
-#   FetchContent_Declare(
-#   fmt
-#   GIT_REPOSITORY https://github.com/fmtlib/fmt.git
-#   GIT_TAG        2cb3b7c64b04fd99b700dc1a40cc5115da549359 # 11.0.2
-#   USES_TERMINAL_DOWNLOAD TRUE
-#   DOWNLOAD_EXTRACT_TIMESTAMP ON
-#   EXCLUDE_FROM_ALL
-#   INACTIVITY_TIMEOUT 30
-#   )
-# FetchContent_MakeAvailable(fmt)
+include(FetchContent)
+  FetchContent_Declare(
+  fmt
+  GIT_REPOSITORY https://github.com/fmtlib/fmt.git
+  GIT_TAG        2cb3b7c64b04fd99b700dc1a40cc5115da549359 # 11.0.2
+  USES_TERMINAL_DOWNLOAD TRUE
+  DOWNLOAD_EXTRACT_TIMESTAMP ON
+  EXCLUDE_FROM_ALL
+  INACTIVITY_TIMEOUT 30
+  )
+FetchContent_MakeAvailable(fmt)
+
+endif()
