42aa634c1a0676cd1b2e968f0171f814fbb260fd
i-see-you-pancake <bugurt2018@yandex.ru>
2024-10-11 22:47:46 +0000

    modified:   CMakeLists.txt 	modified:   cmake/FindBoost.cmake 	modified:   cmake/FindFMT.cmake

--- cmake/FindBoost.cmake
+++ cmake/FindBoost.cmake
@@ -1,14 +1,17 @@
-
 set(CMAKE_FIND_PACKAGE_SORT_ORDER NATURAL)
 set(CMAKE_FIND_PACKAGE_SORT_DIRECTION DEC)
 set(FETCHCONTENT_QUIET FALSE)
 set(BOOST_ENABLE_CMAKE ON)
-
+set(FETCHCONTENT_TRY_FIND_PACKAGE_MODE NEVER)
 
 find_package(Boost 1.81 COMPONENTS program_options json url)
-if(NOT Boost_FOUND)
-
+if(Boost_FOUND)
+  message(STATUS "Found Boost version: ${Boost_VERSION}")
+else()
+  message(STATUS "Boost not found and will be downloaded from git")
+  
   include(FetchContent)
+  
   FetchContent_Declare(
     Boost
     URL https://github.com/boostorg/boost/releases/download/boost-1.86.0/boost-1.86.0-cmake.7z
@@ -18,9 +21,8 @@ if(NOT Boost_FOUND)
     EXCLUDE_FROM_ALL
     INACTIVITY_TIMEOUT 30
   )
-  FetchContent_MakeAvailable(Boost)
 
-else()
-    message(STATUS "Found Boost version: ${Boost_VERSION}")
+FetchContent_MakeAvailable(Boost)
+
 endif()
 
--- cmake/FindFMT.cmake
+++ cmake/FindFMT.cmake
@@ -1,8 +1,6 @@
-
 set(CMAKE_FIND_PACKAGE_SORT_ORDER NATURAL)
 set(CMAKE_FIND_PACKAGE_SORT_DIRECTION DEC)
 set(FETCHCONTENT_QUIET FALSE)
-
 
 find_package(fmt 9.1)
 if(fmt_FOUND)
@@ -11,7 +9,8 @@ else()
   message(STATUS "fmt not found and will be downloaded from git")
 
   include(FetchContent)
-    FetchContent_Declare(
+  
+  FetchContent_Declare(
     fmt
     GIT_REPOSITORY https://github.com/fmtlib/fmt.git
     GIT_TAG        2cb3b7c64b04fd99b700dc1a40cc5115da549359 # 11.0.2
@@ -20,6 +19,7 @@ else()
     EXCLUDE_FROM_ALL
     INACTIVITY_TIMEOUT 30
   )
+  
   FetchContent_MakeAvailable(fmt)
 
 endif()
