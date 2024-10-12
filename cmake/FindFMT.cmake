bdd614acd47094793404853358b73fd9830f6272
Pancake <bugurt2018@yandex.ru>
2024-10-12 12:14:01 +0300

    Add support cmake 3.22.1

--- cmake/FindFMT.cmake
+++ cmake/FindFMT.cmake
@@ -6,32 +6,33 @@ set(FETCHCONTENT_QUIET FALSE)
 if((CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 13.1 AND CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
     OR (CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 17.0.1 AND CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
     OR (CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 19.29 AND CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
-    OR (CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 15.0 AND CMAKE_CXX_COMPILER_ID STREQUAL "AppleClang")
-  )
+    OR (CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 15.0 AND CMAKE_CXX_COMPILER_ID STREQUAL "AppleClang"))
   message("The format library will be used instead of fmt")
-
   else()
-
-  find_package(fmt 9.1)
+  find_package(fmt 8.1)
   if(fmt_FOUND)
     message(STATUS "fmt found version: ${fmt_VERSION}")
   else()
     message(STATUS "fmt not found and will be downloaded from git")
-
     include(FetchContent)
-  
-    FetchContent_Declare(
-      fmt
-      GIT_REPOSITORY https://github.com/fmtlib/fmt.git
-      GIT_TAG        2cb3b7c64b04fd99b700dc1a40cc5115da549359 # 11.0.2
-      USES_TERMINAL_DOWNLOAD TRUE
-      DOWNLOAD_EXTRACT_TIMESTAMP ON
-      EXCLUDE_FROM_ALL
-      INACTIVITY_TIMEOUT 30
-    )
-  
+    if(CMAKE_VERSION VERSION_LESS 3.24)
+      FetchContent_Declare(
+        fmt
+        GIT_REPOSITORY https://github.com/fmtlib/fmt.git
+        GIT_TAG        2cb3b7c64b04fd99b700dc1a40cc5115da549359 # 11.0.2
+        USES_TERMINAL_DOWNLOAD TRUE
+        EXCLUDE_FROM_ALL
+        INACTIVITY_TIMEOUT 30)
+    else()
+      FetchContent_Declare(
+        fmt
+        GIT_REPOSITORY https://github.com/fmtlib/fmt.git
+        GIT_TAG        2cb3b7c64b04fd99b700dc1a40cc5115da549359 # 11.0.2
+        USES_TERMINAL_DOWNLOAD TRUE
+        DOWNLOAD_EXTRACT_TIMESTAMP ON
+        EXCLUDE_FROM_ALL
+        INACTIVITY_TIMEOUT 30)
+    endif()
     FetchContent_MakeAvailable(fmt)
-
   endif()
-
 endif()
