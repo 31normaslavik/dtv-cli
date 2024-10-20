deae843550cafee4fd6b85100a0915736d6f5209
i-see-you-pancake <bugurt2018@yandex.ru>
2024-10-21 00:26:12 +0300

    fix: No target "dtv-cli.exe"

--- cmake/RunCheckTest.cmake
+++ cmake/RunCheckTest.cmake
@@ -1,8 +1,8 @@
 enable_testing()    
 add_test(
   NAME mytest
-  COMMAND $<TARGET_FILE:${PROJECT_NAME}$<$<PLATFORM_ID:Darwin>:_darwin>$<$<PLATFORM_ID:Windows>:.exe>>
-  WORKING_DIRECTORY $<TARGET_FILE_DIR:${PROJECT_NAME}$<$<PLATFORM_ID:Darwin>:_darwin>$<$<PLATFORM_ID:Windows>:.exe>>)
+  COMMAND ${PROJECT_NAME}$<$<PLATFORM_ID:Darwin>:_darwin>$<$<PLATFORM_ID:Windows>:.exe>
+  WORKING_DIRECTORY $<TARGET_FILE_DIR:${PROJECT_NAME}>)
 
   set_property(TEST mytest PROPERTY
   PASS_REGULAR_EXPRESSION "Options;General;Filesystem;Verbosity;Video;Audio;Subtitle;Network;Debug"
