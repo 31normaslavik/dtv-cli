61c9bf284bb15e64d9b5161ba79cb70a38332a8b
i-see-you-pancake <bugurt2018@yandex.ru>
2024-10-22 21:05:42 +0300

    fix: wf; now build static; clean cmakelists files

--- cmake/FindBoost.cmake
+++ cmake/FindBoost.cmake
@@ -4,30 +4,24 @@ set(FETCHCONTENT_QUIET FALSE)
 set(BOOST_ENABLE_CMAKE ON)
 set(FETCHCONTENT_TRY_FIND_PACKAGE_MODE NEVER)
 
-find_package(Boost 1.81 COMPONENTS program_options json url)
-if(Boost_FOUND)
-  message(STATUS "Found Boost version: ${Boost_VERSION}")
+include(FetchContent)
+if(CMAKE_VERSION VERSION_LESS 3.24)
+  FetchContent_Declare(
+    Boost
+    URL https://github.com/boostorg/boost/releases/download/boost-1.86.0/boost-1.86.0-cmake.7z
+    URL_HASH SHA256=ee6e0793b5ec7d13e7181ec05d3b1aaa23615947295080e4b9930324488e078f # 1.86
+    USES_TERMINAL_DOWNLOAD TRUE      
+    EXCLUDE_FROM_ALL
+    INACTIVITY_TIMEOUT 30)  
 else()
-  message(STATUS "Boost not found and will be downloaded from git")
-  include(FetchContent)
-  if(CMAKE_VERSION VERSION_LESS 3.24)
-    FetchContent_Declare(
-      Boost
-      URL https://github.com/boostorg/boost/releases/download/boost-1.86.0/boost-1.86.0-cmake.7z
-      URL_HASH SHA256=ee6e0793b5ec7d13e7181ec05d3b1aaa23615947295080e4b9930324488e078f # 1.86
-      USES_TERMINAL_DOWNLOAD TRUE      
-      EXCLUDE_FROM_ALL
-      INACTIVITY_TIMEOUT 30)  
-  else()
-    FetchContent_Declare(
-      Boost
-      URL https://github.com/boostorg/boost/releases/download/boost-1.86.0/boost-1.86.0-cmake.7z
-      URL_HASH SHA256=ee6e0793b5ec7d13e7181ec05d3b1aaa23615947295080e4b9930324488e078f # 1.86
-      USES_TERMINAL_DOWNLOAD TRUE
-      DOWNLOAD_EXTRACT_TIMESTAMP ON
-      EXCLUDE_FROM_ALL
-      INACTIVITY_TIMEOUT 30)
-  endif()
-  FetchContent_MakeAvailable(Boost)
+  FetchContent_Declare(
+    Boost
+    URL https://github.com/boostorg/boost/releases/download/boost-1.86.0/boost-1.86.0-cmake.7z
+    URL_HASH SHA256=ee6e0793b5ec7d13e7181ec05d3b1aaa23615947295080e4b9930324488e078f # 1.86
+    USES_TERMINAL_DOWNLOAD TRUE
+    DOWNLOAD_EXTRACT_TIMESTAMP ON
+    EXCLUDE_FROM_ALL
+    INACTIVITY_TIMEOUT 30)
 endif()
+FetchContent_MakeAvailable(Boost)
 
--- cmake/FindFMT.cmake
+++ cmake/FindFMT.cmake
@@ -2,37 +2,24 @@ set(CMAKE_FIND_PACKAGE_SORT_ORDER NATURAL)
 set(CMAKE_FIND_PACKAGE_SORT_DIRECTION DEC)
 set(FETCHCONTENT_QUIET FALSE)
 
-
-if((CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 13.1 AND CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
-    OR (CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 17.0.1 AND CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
-    OR (CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 19.29 AND CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
-    OR (CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 15.2 AND CMAKE_CXX_COMPILER_ID STREQUAL "AppleClang"))
-  message("The <format> library will be used instead of fmt")
+message(STATUS "fmt not found and will be downloaded from git")
+include(FetchContent)
+if(CMAKE_VERSION VERSION_LESS 3.24)
+  FetchContent_Declare(
+    fmt
+    GIT_REPOSITORY https://github.com/fmtlib/fmt.git
+    GIT_TAG        2cb3b7c64b04fd99b700dc1a40cc5115da549359 # 11.0.2
+    USES_TERMINAL_DOWNLOAD TRUE
+    EXCLUDE_FROM_ALL
+    INACTIVITY_TIMEOUT 30)
 else()
-  find_package(fmt 8.1)
-  if(fmt_FOUND)
-    message(STATUS "fmt found version: ${fmt_VERSION}")
-  else()
-    message(STATUS "fmt not found and will be downloaded from git")
-    include(FetchContent)
-    if(CMAKE_VERSION VERSION_LESS 3.24)
-      FetchContent_Declare(
-        fmt
-        GIT_REPOSITORY https://github.com/fmtlib/fmt.git
-        GIT_TAG        2cb3b7c64b04fd99b700dc1a40cc5115da549359 # 11.0.2
-        USES_TERMINAL_DOWNLOAD TRUE
-        EXCLUDE_FROM_ALL
-        INACTIVITY_TIMEOUT 30)
-    else()
-      FetchContent_Declare(
-        fmt
-        GIT_REPOSITORY https://github.com/fmtlib/fmt.git
-        GIT_TAG        2cb3b7c64b04fd99b700dc1a40cc5115da549359 # 11.0.2
-        USES_TERMINAL_DOWNLOAD TRUE
-        DOWNLOAD_EXTRACT_TIMESTAMP ON
-        EXCLUDE_FROM_ALL
-        INACTIVITY_TIMEOUT 30)
-    endif()
-    FetchContent_MakeAvailable(fmt)
-  endif()
+  FetchContent_Declare(
+    fmt
+    GIT_REPOSITORY https://github.com/fmtlib/fmt.git
+    GIT_TAG        2cb3b7c64b04fd99b700dc1a40cc5115da549359 # 11.0.2
+    USES_TERMINAL_DOWNLOAD TRUE
+    DOWNLOAD_EXTRACT_TIMESTAMP ON
+    EXCLUDE_FROM_ALL
+    INACTIVITY_TIMEOUT 30)
 endif()
+FetchContent_MakeAvailable(fmt)
